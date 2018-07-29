ad3e05d4fec06e98ceb3922dde54863c9d58bd5c
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-07-29 00:21:41 -0500

    card.js and robot.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import robots from './robots';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+/* 
+ReactDOM.render(<Card />, document.getElementById('root'));
+registerServiceWorker(); 
+
+-because I want more than one Card, I will rewrite the same code as above, as follows: */
+
+ReactDOM.render(
+   <div>
+      <Card />
+      <Card />
+      <Card />
+   </div>
+, document.getElementById('root'));
+registerServiceWorker(); 
