19760420cf6ef7821c24928781e09c8e5b581d16
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-07-29 01:34:37 -0500

    cardsArray

--- src/CardList.js
+++ src/CardList.js
@@ -12,13 +12,40 @@ const CardList = ({ robots }) => {
    );
 }; */
 
+/*
 const CardList = ({ robots }) => {
    const cardsArray = robots.map((user, i) => {
-      return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+      return (
+         <Card 
+            key={i} 
+            id={robots[i].id} 
+            name={robots[i].name} 
+            email={robots[i].email}
+         />
+      );
    });
    return (
       <div>
          {cardsArray}
+      </div>
+   );
+}; */
+
+const CardList = ({ robots }) => {
+   return (
+      <div>
+      {
+         robots.map((user, i) => {
+            return (
+               <Card 
+                  key={i} 
+                  id={robots[i].id} 
+                  name={robots[i].name} 
+                  email={robots[i].email}
+               />
+            );
+         })
+      }
       </div>
    );
 };
