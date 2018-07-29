2217c5b45a58f4d724124d5a8275ac27d7ba77ce
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-07-29 01:20:17 -0500

    cardsArray

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import { robots } from './robots';
@@ -14,10 +14,6 @@ registerServiceWorker();
 -because I want more than one Card, I will rewrite the same code as above, as follows: */
 
 ReactDOM.render(
-   <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-   </div>
+   <CardList robots={robots}/>
 , document.getElementById('root'));
 registerServiceWorker(); 
