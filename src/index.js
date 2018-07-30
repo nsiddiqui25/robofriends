b58130b841faffc28641fbb08628e805e8fc467b
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-07-29 20:09:44 -0500

    add searchBox component and make it interactive

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { robots } from './robots';
-
 
 /* 
 ReactDOM.render(<Card />, document.getElementById('root'));
@@ -13,7 +11,5 @@ registerServiceWorker();
 
 -because I want more than one Card, I will rewrite the same code as above, as follows: */
 
-ReactDOM.render(
-   <CardList robots={robots}/>
-, document.getElementById('root'));
+ReactDOM.render(<App /> , document.getElementById('root'));
 registerServiceWorker(); 
