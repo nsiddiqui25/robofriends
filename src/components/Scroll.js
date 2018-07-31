f4f0e758e60819fa948753187464610632093ae4
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-07-30 22:30:42 -0500

    reorganize files and clean up code

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 // scrollable component
 const Scroll = (props) => {
    return (
-      <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px' }}>
+      <div style={{ overflowY: 'scroll', border: '5px solid yellow', height: '800px' }}>
          {props.children}
       </div>
    );
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
       super()
       this.state = {
          robots: [],
-         searchfield: '' 
+         searchfield: ''
       }
       // console.log('constructor');
    }
@@ -26,14 +26,14 @@ class App extends Component {
    }
 
    render() {
-      const filteredRobots = this.state.robots.filter(robots => {
-         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+      const { robots, searchfield } = this.state;
+      const filteredRobots = robots.filter(robot => {
+         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
       })
       // console.log('render');
-      if (this.state.robots.length === 0) {
-         return <h1>Loading your bots</h1>
-      } else {
-         return (
+      return !robots.length ?
+         <h1>Loading your bots</h1> :
+         (
             <div className='tc'>
                <h1 className='f1'>RoboFriends</h1>
                <SearchBox searchChange={this.onSearchChange} />
@@ -42,7 +42,6 @@ class App extends Component {
                </Scroll>
             </div>
          );
-      }
    }
 }
 
