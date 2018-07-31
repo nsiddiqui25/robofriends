e76af7650cc33b13dc2bb4f1b3672b6db2090896
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-07-30 22:13:36 -0500

    build scrollabe component

---
+++ src/Scroll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// scrollable component
+const Scroll = (props) => {
+   return (
+      <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px' }}>
+         {props.children}
+      </div>
+   );
+};
+
+export default Scroll;
