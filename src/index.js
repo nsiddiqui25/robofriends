b79894fa7b393fe3ce1654f9a2d717cb0876c875
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-08-06 20:38:27 -0500

    redux store and provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
+const store = createStore(searchRobots)
 /* 
 ReactDOM.render(<Card />, document.getElementById('root'));
 registerServiceWorker(); 
 
 -because I want more than one Card, I will rewrite the same code as above, as follows: */
 
-ReactDOM.render(<App /> , document.getElementById('root'));
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>, document.getElementById('root'));
 registerServiceWorker(); 
