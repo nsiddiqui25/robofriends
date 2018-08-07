edff12b73230f79d860a0e289fe1fb42749fc65b
Nabeel Siddiqui <n.siddiqui25@gmail.com>
2018-08-06 22:37:34 -0500

    redux middleware

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,7 +10,6 @@ import { setSearchField } from '../actions';
 
 const mapStateToProps = state => {
    return {
-      // searchField: state.searchRobots.searchField
       searchField: state.searchField
    }
 }
@@ -26,33 +25,21 @@ class App extends Component {
       super()
       this.state = {
          robots: []
-         // searchfield: ''
       }
-      // console.log('constructor');
    }
 
    componentDidMount() {
-      // console.log(this.props.store)
       fetch('https://jsonplaceholder.typicode.com/users')
          .then(response => response.json())
          .then(users => this.setState({ robots: users }));
-      // console.log('componentDidMount');
    }
 
-/* onSearchChange = (event) => {
-      this.setState({ searchfield: event.target.value })
-   }  
-      //it's now coming down as props after incorporating Redux; so we don't need to declare onSearchChange as a method of App.js so get rid of it.
-*/
-
    render() {
-      // const { robots, searchfield } = this.state;
       const { robots } = this.state;
       const { searchField, onSearchChange } = this.props;
       const filteredRobots = robots.filter(robot => {
          return robot.name.toLowerCase().includes(searchField.toLowerCase());
       })
-      // console.log('render');
       return !robots.length ?
          <h1>Loading your bots</h1> :
          (
